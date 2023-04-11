7a53a5215df84192d86b5a500df3e3d1664bf0b9
Aesha1995 <aeshap2905@gmail.com>
2023-04-11 23:17:10 +0100

    com-saucedemo

---
+++ src/com_sourcedemo/ChromeBrowserTesting.java
@@ -0,0 +1,19 @@
+package com_sourcedemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowserTesting {
+    public static void main(String[] args) {
+        String baseUrl = "https://www.saucedemo.com/";
+        WebDriver driver = new ChromeDriver(); // launch Chrome browser
+        driver.get(baseUrl); //open url
+        System.out.println("Title of Website is : " + driver.getTitle()); //print title of the website
+        System.out.println("URL of current webpage is : " +driver.getCurrentUrl()); //print current url of webpage
+        System.out.println("Source code of webpage : " +driver.getPageSource()); //print source code of webpage
+        driver.findElement(By.id("user-name")).sendKeys("Aesha123@gmail.com"); //it will enter email id in email field
+        driver.findElement(By.id("password")).sendKeys("Abcde12345"); //it will enter password in password field
+        driver.close(); //it will close browser
+    }
+}
